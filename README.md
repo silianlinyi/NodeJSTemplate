2e0a18500d41a9ed23d8f7a9b08b6892e63738b9
silianlinyi <244098979@qq.com>
2013-10-20 19:25:04 +0800

    add

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ NodeJS项目模板
 
 >     public/
 >       |-- css		存放项目的css文件
->	    |-- fonts	存放项目的字体文件
->	    |-- ico		存放项目的ico文件
+>	    |-- font	存放项目的字体文件
+>	    |-- icon	存放项目的icon文件
 >	    |-- img		存放项目的图片文件
 >	    |-- js 		存放项目的js文件
->	    |-- lib		存放jquery.js等通用类库
+>	    |-- lib		存放js第三方类库
 >	    |-- seajs	存放sea.js及其插件
--- app.js
+++ app.js
@@ -2,19 +2,29 @@
 /**
  * Module dependencies.
  */
+var express = require('express'),
+	http = require('http'),
+	path = require('path'),
+	mongoose = require('mongoose'),
+	config = require('./config'),
+	routes = require('./routes');
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+// 数据库连接
+mongoose.connect('mongodb://' + config.MONGODB_IP + '/' + config.MONGODB_DATABASE_NAME, function(err) {
+	if(!err) {
+		console.log('【日志】连接到数据库：' + config.MONGODB_DATABASE_NAME);
+	} else {
+		throw err;
+	}
+});
 
 var app = express();
-
+// 修改文件后缀
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || config.WEB_SERVER_PORT);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -29,8 +39,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+// 路由
+routes(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
